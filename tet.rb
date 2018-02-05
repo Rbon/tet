d4e862a56faa45fbb888a5224f41604d2d2ce766
Robin Bisho <robinbisho@gmail.com>
2018-02-04 18:16:23 -0800

    switched dimensions

--- tet.rb
+++ tet.rb
@@ -18,20 +18,32 @@ class Game
     @renderer = opts[:renderer]
     @input = opts[:input]
     @running = true
+    @g_num = 0
   end
 
   def run
     begin
       @renderer.start
-      @renderer.draw(@play_field)
-      @current_block = OBlock.new
+      @current_block = ZBlock.new
       update_grid
+      @renderer.draw(@play_field)
       while @running
+        key = @input.manage
+        act(key)
+        apply_gravity
         @renderer.draw(@play_field)
-        act(@input.manage)
       end
+      sleep(1/60)
     ensure
       @renderer.stop
+    end
+  end
+
+  def apply_gravity
+    @g_num += 1
+    if @g_num == 60
+      move_down
+      @g_num = 0
     end
   end
 
@@ -41,11 +53,17 @@ class Game
 
   def move_left
     update_grid(0)
-    @current_block.pos[1] -= 1
+    @current_block.pos[0] -= 1
     update_grid
   end
 
   def move_right
+    update_grid(0)
+    @current_block.pos[0] += 1
+    update_grid
+  end
+
+  def move_down
     update_grid(0)
     @current_block.pos[1] += 1
     update_grid
@@ -55,8 +73,12 @@ class Game
     @current_block.data.each do |cell|
       x_pos = cell[1][0] + @current_block.pos[0]
       y_pos = cell[1][1] + @current_block.pos[1]
-      @play_field[x_pos][y_pos] = override || cell[0]
+      @play_field[y_pos][x_pos] = override || cell[0]
     end
+  end
+
+  def stop
+    @running = false
   end
 end
 
@@ -72,12 +94,23 @@ class OBlock < Block
   end
 end
 
+class ZBlock < Block
+  def initialize
+    @data = [
+      [1, [0, 0]], [1, [1, 0]], [0, [2, 0]],
+      [0, [0, 1]], [1, [1, 1]], [1, [2, 1]]
+    ]
+    @pos = [5, 5]
+  end
+end
+
 class Input
   def initialize(opts)
     @source = opts[:source]
     @map = {
       move_left: "h",
-      move_right: "l"
+      move_right: "l",
+      stop: "q"
     }.invert
   end
 
@@ -93,8 +126,11 @@ class Renderer
     Curses.noecho # Don't display pressed characters
     @window = Curses::Window.new(42, 22, 0, 0)
     @window.box("|", "-")
-    @window.keypad=(true)
+    @window.keypad = true
+    @window.timeout = 10
     @map = ["  ", "OO"]
+    @dev_window = Curses::Window.new(30, 30, 0, 30)
+    @dev_window.setpos(1, 1)
   end
 
   def stop
@@ -103,14 +139,22 @@ class Renderer
 
   def draw(grid)
     y_pos = 0
+    @window.setpos(0, 1)
     grid.each do |row|
       2.times do
         y_pos += 1
         @window.setpos(y_pos, 1)
-        row.each { |cell| @window.addstr(@map[cell]) }
+        row.each do |cell|
+          @window.addstr(@map[cell])
+        end
       end
     end
     @window.refresh
+  end
+
+  def dev_draw(output)
+    @dev_window.addstr(output.to_s + "\n")
+    @dev_window.refresh
   end
 
   def input
